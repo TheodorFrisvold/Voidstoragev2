b72a9ab58d291d3cd5db73fc9787208579ae628a
BuildTools <unconfigured@null.spigotmc.org>
2024-03-09 14:46:16 +0100

    Removed unnecessary code

--- src/main/java/dev/favn/voidstorage/events/UseVoid.java
+++ src/main/java/dev/favn/voidstorage/events/UseVoid.java
@@ -5,7 +5,6 @@ import dev.favn.voidstorage.itemfactory.FormedVoid;
 import dev.favn.voidstorage.utility.KeyCache;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,18 +15,10 @@ import org.bukkit.inventory.ItemStack;
 public class UseVoid implements Listener {
 
     private static VoidStorage _plugin;
-    private static KeyCache _keyCache;
-    private static NamespacedKey key;
-    private static NamespacedKey amountKey;
-    private static NamespacedKey maxKey;
 
 
     public UseVoid(VoidStorage plugin, KeyCache keyCache){
         _plugin = plugin;
-        _keyCache = keyCache;
-        key = _keyCache.getKey("formedvoid_type");
-        amountKey = _keyCache.getKey("formedvoid_amount");
-        maxKey = _keyCache.getKey("formedvoid_max");
         this._plugin.getServer().getPluginManager().registerEvents(this, this._plugin);
         _plugin.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "Registered PlaceWithVoid class properly");
     }
